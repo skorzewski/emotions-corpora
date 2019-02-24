bdf5ff8dcd627c4006446c5e94d59651c4064b09
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2019-02-24 09:59:49 +0100

    Batches and annotation script

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # emotions-corpora
 
 Corpora for emotion analysis.
+
+You can help annotating text batches using provided script `annotate.py`:
+```
+./annotate.py batch<BATCH_NO>.txt
+```
+e.g.:
+```
+./annotate.py batch1.txt
+```
---
+++ annotate.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import random
+import re
+import sys
+
+
+RE_OTHER = re.compile(r'[^GLNORSUWZ]')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='''Annotate utterances with emotions.\n
+        Usage:\n
+        ./annotate.py batch1.txt
+        ''')
+    parser.add_argument(
+        'infile',
+        nargs='?',
+        type=argparse.FileType('r'),
+        default=sys.stdin,
+        help='Input file with utterances',
+    )
+    parser.add_argument(
+        'outfile',
+        nargs='?',
+        type=argparse.FileType('w'),
+        default=None,
+        help='Output TSV file',
+    )
+    return parser.parse_args()
+
+
+def main(infile, outfile):
+    utterances = infile.readlines()
+    total = len(utterances)
+    for i, utt in enumerate(utterances):
+        progress = i * 20 // total
+        progress_bar = progress * '#' + (20 - progress) * ' '
+        print('[{}] Zdanie {} z {}:'.format(progress_bar, i + 1, total))
+        print()
+        print('"{}"'.format(utt.strip()))
+        print()
+        print('Która z poniższych emocji opisuje powyższe zdanie? Wybierz jedną lub więcej i zatwierdź klawiszem ENTER:')
+        print('R – Radość   U – Ufność   L – Lęk     Z – Zaskoczenie')
+        print('S – Smutek   W – Wstręt   G – Gniew   O – Oczekiwanie')
+        print('N – wypowiedź jest Neutralna')
+        print()
+        letters = ''.join(sorted(c for c in RE_OTHER.sub('', input().upper())))
+        print('{}\t{}'.format(letters, utt), file=outfile)
+        print()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    if not args.outfile:
+        outfile_id = random.randint(0, 10000)
+        outfile_name = '{}.{:04}.tsv'.format(args.infile.name, outfile_id)
+        with open(outfile_name, 'w') as outfile:
+            main(args.infile, outfile)
+    else:
+        main(args.infile, args.outfile)
